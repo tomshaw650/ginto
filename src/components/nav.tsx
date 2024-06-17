3c6c1cd9dab5a186ca2cf6a6036b47536149682a
Tom Shaw <tomshaw650@gmail.com>
2024-06-17 17:07:39 +0100

    show nav on mobile

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useMediaQuery } from "usehooks-ts";
 import { ThemeToggle } from "./theme-toggle";
 import SignoutButton from "./sign-out";
 
@@ -15,6 +16,28 @@ interface NavHeaderProps {
 }
 
 export default function NavHeader(props: NavHeaderProps) {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  if (isDesktop) {
+    return (
+      <header className="flex h-20 w-screen flex-row items-center justify-center">
+        <nav className="flex items-center space-x-6">
+          {!props.landing ? (
+            <>
+              {navLinks.map((link) => (
+                <Link key={link.name} href={link.href}>
+                  {link.name}
+                </Link>
+              ))}
+            </>
+          ) : null}
+          <ThemeToggle />
+          {!props.landing && <SignoutButton />}
+        </nav>
+      </header>
+    );
+  }
+
   return (
     <header className="flex h-20 w-screen flex-row items-center justify-center">
       <nav className="flex items-center space-x-6">
@@ -27,8 +50,6 @@ export default function NavHeader(props: NavHeaderProps) {
             ))}
           </>
         ) : null}
-        <ThemeToggle />
-        {!props.landing && <SignoutButton />}
       </nav>
     </header>
   );
