6f160a4b0b55f7ef285cbc76d9e2b1168063a33a
Tom Shaw <tomshaw650@gmail.com>
2024-06-17 17:17:26 +0100

    rollback nav and light theme default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <ReactQueryProvider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,6 +1,4 @@
-"use client";
 import Link from "next/link";
-import { useMediaQuery } from "usehooks-ts";
 import { ThemeToggle } from "./theme-toggle";
 import SignoutButton from "./sign-out";
 
@@ -17,28 +15,6 @@ interface NavHeaderProps {
 }
 
 export default function NavHeader(props: NavHeaderProps) {
-  const isDesktop = useMediaQuery("(min-width: 768px)");
-
-  if (isDesktop) {
-    return (
-      <header className="flex h-20 w-screen flex-row items-center justify-center">
-        <nav className="flex items-center space-x-6">
-          {!props.landing ? (
-            <>
-              {navLinks.map((link) => (
-                <Link key={link.name} href={link.href}>
-                  {link.name}
-                </Link>
-              ))}
-            </>
-          ) : null}
-          <ThemeToggle />
-          {!props.landing && <SignoutButton />}
-        </nav>
-      </header>
-    );
-  }
-
   return (
     <header className="flex h-20 w-screen flex-row items-center justify-center">
       <nav className="flex items-center space-x-6">
@@ -51,6 +27,8 @@ export default function NavHeader(props: NavHeaderProps) {
             ))}
           </>
         ) : null}
+        <ThemeToggle />
+        {!props.landing && <SignoutButton />}
       </nav>
     </header>
   );
